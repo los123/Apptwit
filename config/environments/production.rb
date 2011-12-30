4ab4837283e84ee65681a731bafc28fbc1d6732d
los123 <elijah.lovkoff@gmail.com>
2011-12-30 18:38:18 -0500

    changed production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,8 +15,9 @@ Apptwit::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
-
+  # config.assets.compile = false
+  # commented out trying to solve some heroku issues, chenged belowe to true
+  config.assets.compile = true
   # Generate digests for assets URLs
   config.assets.digest = true
 
